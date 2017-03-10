39776621b33d9b169f59e1060cf8f24f6af56b44
Vedant Sareen <vedant.sareen@students.iiit.ac.in>
2017-03-10 14:46:15 +0530

    Started making the first level of Super Mario
    Added background, bricks, a model and a following camera

---
+++ Super Mario Bros/Assets/Scripts/BlockNonDestroy.js
@@ -0,0 +1,14 @@
+﻿var XPos : float = transform.position.x;
+var YPos : float = transform.position.y;
+var ZPos : float = transform.position.z;
+
+function OnTriggerEnter (col : Collider) {
+	transform.GetComponent.<Collider>().isTrigger = false;
+	if (col.GameObject.tag == "Player") {
+		this.transform.position = Vector3(XPos, YPos+0.2, ZPos);
+		yield WaitForSeconds(0.08);
+		this.transform.position = Vector3(XPos, YPos+0.2, ZPos);
+		yield WaitForSeconds(0.25);
+		transform.GetComponent.<Collider>().isTrigger = true;
+	}
+}
--- Super Mario Bros/Assets/Scripts/CameraFollow.js
+++ Super Mario Bros/Assets/Scripts/CameraFollow.js
@@ -1,9 +1,12 @@
-﻿#pragma strict
-
-function Start () {
-	
-}
+﻿var Player : Transform;
+var DistanceFromPlayer : float = 5;
+var StaticCameraY : float = 3;
 
 function Update () {
-	
+	transform.position.z = Player.position.z - DistanceFromPlayer;
+	transform.position.x = Player.position.x;
+}
+
+function LateUpdate () {
+	GetComponent.<Camera>().main.transform.position.y = StaticCameraY;
 }
